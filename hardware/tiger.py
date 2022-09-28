b402be933a441014ed171f1dabac418ea9e0af1d
alecgao066 <gan_gao@outlook.com>
2022-09-28 02:28:34 -0700

    linting and doc string for tiger controller

--- hardware/tiger.py
+++ hardware/tiger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-## @file
+# @file
 """
 RS232 interface to a Applied Scientific Instrumentation MS2000 stage.
 
@@ -10,28 +10,27 @@ Kevin Bishop 2/22 - changed to be for Tiger controller instead.
 Added set acceleration on 5/6/22 -KB
 Renamed variables to refer to mm and not um (code is correct,
     but names/comments weren't) on 5/9/22 -KB
-
+Gan Gao 09/22: enforced linting and added doc strings.
 """
-import sys 
+
 import hardware.RS232 as RS232
-import time
 
-## TIGER
-#
-# Applied Scientific Instrumentation tiger RS232 interface class.
-#
+
 class TIGER(RS232.RS232):
-
-    ## __init__
-    #
-    # Connect to the Tiger stage at the specified port.
-    #
-    # @param port The RS-232 port name (e.g. "COM1").
-    # @param wait_time (Optional) How long (in seconds) for a response from the stage.
-    #
+    """
+    Applied Scientific Instrumentation Tiger RS232 interface class.
+    """
     def __init__(self, **kwds):
+        """
+        Connect to the Tiger stage at the specified port.
 
-        self.mm_to_unit = 10000    #10000 means "10000 units = 1mm"
+        Parameters:
+        port: The RS-232 port name (e.g. "COM1").
+        wait_time (Optional): How long (in seconds) for a response from
+        the stage.
+        """
+
+        self.mm_to_unit = 10000  # 10000 means "10000 units = 1mm"
         self.unit_to_mm = 1.0/self.mm_to_unit
         self.x = 0.0
         self.y = 0.0
@@ -40,31 +39,41 @@ class TIGER(RS232.RS232):
         try:
             # open port
             super().__init__(**kwds)
-        except:
+        except Exception:
             print("ASI Stage is not connected? Stage is not on?")
 
     def getCD(self):
+        """
+        Returns the date and time the current firmware were compiled.
+        """
         response = self.commWithResp("CD")
         return response
 
     def getBU(self):
+        """
+        Returns various build configuration options, the firmware modules
+        in the build, and the build name.
+        """
         response = self.commWithResp("BU X")
         return response
 
-    ## getMotorStatus
-    #
-    # @return True/False if the stage is moving.
-    #
     def getMotorStatus(self):
+        """
+        Return True/False if the stage is moving.
+        """
         response = self.commWithResp("/")
         return response
 
-    ## goAbsolute
-    #
-    # @param axis - X, Y, or Z.
-    # @param pos position in mm
-    #
     def goAbsolute(self, axis, pos, bwait):
+        """
+        Move to an absolute coordinate.
+
+        Parameters:
+        axis: X, Y, or Z.
+        pos: absolute position coordinates in mm.
+        bwait (bool): If true, execute this movement command until
+        there are no motors running from a serial command.
+        """
         p = pos * self.mm_to_unit
         p = round(p)
         self.commWithResp("M " + axis + "=" + str(p))
@@ -73,122 +82,144 @@ class TIGER(RS232.RS232):
             while response[0] == 'B':
                 response = self.getMotorStatus()
 
-    ## goRelative
-    #
-    # @param x Amount to move the stage in x in mm.
-    # @param y Amount to move the stage in y in mm.
-    #
     def goRelative(self, axis, pos, bwait):
+        """
+        Move to a relative coordinate.
+
+        Parameters:
+        axis: X, Y, or Z.
+        pos: Amount to move the stage in mm.
+        bwait (bool): If true, execute this movement command until
+        there are no motors running from a serial command.
+        """
         p = pos * self.mm_to_unit
         p = round(p)
         self.commWithResp("R " + axis + "=" + str(p))
-        if bwait == True:
+        if bwait is True:
             response = self.getMotorStatus()
             while response[0] == 'B':
                 response = self.getMotorStatus()
 
-    ## getPosition
-    #
-    # @return [stage x (mm), stage y (mm), stage z (mm)]
-    #
     def getPosition(self):
+        """
+        Return [stage x (mm), stage y (mm), stage z (mm)]
+        """
         try:
             [self.x, self.y, self.z] = self.commWithResp("W X Y Z").split(" ")[1:4]
             self.x = float(self.x)*self.unit_to_mm # convert to mm
             self.y = float(self.y)*self.unit_to_mm # convert to mm
             self.z = float(self.z)*self.unit_to_mm # convert to mm
-        except:
+        except Exception:
             print("Stage Error")
         return [self.x, self.y, self.z]
 
-    ## setBacklash
-    #
-    # @param backlash. 0 (off) or 1 (on)
-    #
     def setBacklash(self, axis, backlash):
+        """
+        Set the amount of distance in millimeters to travel to
+        absorb the backlash in the axis' gearing.
+
+        Parameters:
+        axis: X, Y, or Z.
+        backlash: Distance to travel in millimeters. 0 (off).
+        """
         self.commWithResp("B " + axis + "=" + str(backlash))
 
-    ## scan
-    #
-    # @param activate stage scan
-    #
-    def scan(self,bwait):
+    def scan(self, bwait):
+        """
+        Activate stage scan.
+
+        Parameters:
+        bwait (bool): If true, execute scanning until
+        there are no motors running from a serial command.
+        """
         self.commWithResp("SCAN")
-        if bwait == True:
+        if bwait is True:
             response = self.getMotorStatus()
             while response[0] == 'B':
                 response = self.getMotorStatus()
 
-    ## setScanF
-    #
-    # @param f. 0 - RASTER, 1 - SERPENTINE
-    #
     def setScanF(self, x):
+        """
+        Sets up scanning pattern.
+
+        Parameters:
+        x: 0 for RASTER scans or 1 for SERPENTINE scans.
+        """
         self.commWithResp("SCAN F=" + str(x))
 
-    ## setScanR
-    #
-    # @param x. START (in mm)
-    # @param y. STOP (in mm)
-    # @param z. ENC_DIVIDE
-    #
     def setScanR(self, x, y):
-        x = round(x,3)
-        y = round(y,3)
+        """
+        Sets up raster scan start and stop positions.
+
+        Parameters:
+        x: Start position.
+        y: Stop position.
+        """
+        x = round(x, 3)
+        y = round(y, 3)
         self.commWithResp("SCANR X=" + str(x) + " Y=" + str(y))
 
-    ### ****May need to be updated for F param: http://asiimaging.com/docs/products/tiger#commandscanv_nv****
-    ## setScanV
-    #
-    # @param x. START (in mm)
-    # @param y. STOP (in mm)
-    # @param z. NUMBER OF LINES
-    # @param f. OVERSHOOT FACTOR
-    #
     def setScanV(self, x):
-        x = round(x,3)
-        self.commWithResp("SCANV X=" + str(x) + "Y=" + str(x) + " Z=1 F=10")
-        #may need to adjust F number, it is the extra settling time in ms
+        """
+        Set up the slow-scan (vertical) start and stop positions.
 
-    ## setTTL
-    #
-    # @param card. card number (1, 2, 3, etc.)
-    # @param axis. X, Y, or Z.
-    # @param TTL. 0 (on) or 1 (off)
-    #
+        Parameters:
+        x: z position.
+        """
+        x = round(x, 3)
+        #may need to adjust F number, it is the extra settling time in ms
+        self.commWithResp("SCANV X=" + str(x) + "Y=" + str(x) + " Z=1 F=10")
+
     def setTTL(self, card, axis, ttl):
+        """
+        Set TTL input (IN0) and output (OUT0) port.
+
+        Parameters:
+        card: card number (1, 2, 3, etc.)
+        axis: X, Y, or Z.
+        ttl: 0 (off) or 1 (on).
+        """
         self.commWithResp(str(card) + "TTL " + axis + "=" + str(ttl))
 
-    ## setPLCPreset
-    #
-    # @param card. card number (1, 2, 3, etc.)
-    # @param preset. preset PLC code - use 52 for stage SYNC access on BNC 3
-    #
     def setPLCPreset(self, card, preset):
+        """
+        Set the configuration flags as https://asiimaging.com/docs/commands/customa.
+
+        Parameters:
+        card: card number (1, 2, 3, etc.)
+        preset: preset PLC code - use 52 for stage SYNC access on BNC 3.
+        """
         self.commWithResp(str(card) + "CCA X=" + str(preset))
 
-    ## setAcceleration
-    #
-    # @param axis. X, Y, or Z.
-    # @param accel time (ms) to reach velocity.
-    #
     def setAcceleration(self, axis, accel):
+        """
+        Set the amount of time in milliseconds that it takes
+        an axis motor speed to go from stopped to the set speed.
+        It is also the duration of the deceleration time at the
+        end of the move.
+
+        Parameters:
+        axis: X, Y, or Z.
+        accel: Acceleration time to reach velocity in ms.
+        """
         self.commWithResp("AC " + axis + "=" + str(accel))
-        
-    ## setVelocity
-    #
-    # @param axis. X, Y, or Z.
-    # @param vel Maximum velocity (mm/s).
-    #
+      
     def setVelocity(self, axis, vel):
-        vel = round(vel,5)
+        """
+        Set the speed at which the stage will move
+        during the middle of a commanded move.
+
+        Parameters:
+        axis: X, Y, or Z.
+        vel: Maximum velocity to move in mm/s.
+        """
+        vel = round(vel, 5)
         self.commWithResp("S " + axis + "=" + str(vel))
 
-    ## zero
-    #
-    # Set the current stage position as the stage zero.
-    #
     def zero(self):
+        """
+        Set the current stage position as the stage zero.
+        """
         self.commWithResp("Z")
 
 #
